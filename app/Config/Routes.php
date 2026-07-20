02b6277a34354df9c3705c4480b52949f51db983
Miangola <miangolarandriamiadana@gmail.com>
2026-07-20 12:20:06 +0300

    Add missing client routes (deposit, withdraw, transfer, history)

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -10,6 +10,15 @@ $routes->post('/client/login', 'Client::login');
 $routes->get('/client/dashboard', 'Client::dashboard');
 $routes->get('/client/logout', 'Client::logout');
 
+// Client routes
+$routes->get('/client/deposit', 'Client::deposit');
+$routes->post('/client/process-deposit', 'Client::processDeposit');
+$routes->get('/client/withdraw', 'Client::withdraw');
+$routes->post('/client/process-withdraw', 'Client::processWithdraw');
+$routes->get('/client/transfer', 'Client::transfer');
+$routes->post('/client/process-transfer', 'Client::processTransfer');
+$routes->get('/client/history', 'Client::history');
+
 // Admin routes
 $routes->get('/admin', 'Admin::index');
 $routes->get('/admin/prefixes', 'Admin::prefixes');
