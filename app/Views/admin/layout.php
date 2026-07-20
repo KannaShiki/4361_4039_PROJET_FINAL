217f68dd4d478de3adf337bdde454398bc02cb35
Miangola <miangolarandriamiadana@gmail.com>
2026-07-20 12:30:23 +0300

    Add custom teal color scheme design - simple and clean UI

--- app/Views/admin/layout.php
+++ app/Views/admin/layout.php
@@ -6,6 +6,7 @@
     <title><?= isset($title) ? $title . ' - ' : '' ?>Admin Mobile Money</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.0/font/bootstrap-icons.css">
+    <link href="<?= base_url('/assets/css/style.css') ?>" rel="stylesheet">
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
